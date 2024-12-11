3c2859b80d9f385926fba1d23bca075ecd275f71
Joerg Heseler <joergheseler@gmx.de>
2024-12-11 11:22:23 +0100

    Update gltf-validator.py

--- src/gltf-validator.py
+++ src/gltf-validator.py
@@ -25,6 +25,8 @@ def parse_gltf_validator_data(target):
     args = [validator_path, '-amo', target]
     try:
         output = subprocess.check_output(args).decode("utf8")
+    except FileNotFoundError:
+        raise GLTFValidatorException("gltf_validator not found. Use --validator-path= to specify its path.")
     except subprocess.CalledProcessError:
         raise GLTFValidatorException("gltf_validator failed when running: " + ' '.join(args))
     return json.loads(output.encode("utf8"))
